3abe6dbdd054677020d81fd422942d79dd2a2d58
yutingLei <lyt_930617@163.com>
2018-11-27 14:20:15 +0800

    Initial Commit

---
+++ Expander/Expander.h
@@ -0,0 +1,19 @@
+//
+//  Expander.h
+//  Expander
+//
+//  Created by admin on 2018/11/27.
+//  Copyright © 2018 Develop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Expander.
+FOUNDATION_EXPORT double ExpanderVersionNumber;
+
+//! Project version string for Expander.
+FOUNDATION_EXPORT const unsigned char ExpanderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Expander/PublicHeader.h>
+
+
